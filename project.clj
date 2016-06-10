8ba833af02217c77350b371ed96bbfa85d8a2efd
Christian Romney <cromney@pointslope.com>
2016-06-10 10:00:38 -0400

    Leiningen is way easier
    Signed-off-by: Christian Romney <cromney@pointslope.com>

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject pointslope/remit "0.2.0"
+  :description "A tiny event pub/sub library built on core.async"
+  :url "https://github.com/pointslope/remit"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.36"]
+                 [org.clojure/core.async "0.2.374" ]]
+  :repositories    [["clojars" {:url "https://clojars.org/repo/"
+                                :creds :gpg}]]
+  :deploy-repositories [["releases" :clojars]]
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
+
